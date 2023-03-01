72e1bb850e8a06c314794335b198558be16c3bf7
YoungFlyme <youngflyme@web.de>
2023-03-01 16:36:17 +0100

    Updated Global.d.ts to declare truly global functions. Fixes #1

--- PixInsightDefinitions/Global.d.ts
+++ PixInsightDefinitions/Global.d.ts
@@ -1,104 +1,37 @@
-//-------------------------
-//--------Constants--------
-//-------------------------
-
-
-//-------------------------
-//----------Class----------
-//-------------------------
-declare class Global {
-
-
-    //-------------------------
-    //-------Properties--------
-    //-------------------------
-
-
-    //-------------------------
-    //------Constructors-------
-    //-------------------------
-
-
-    //-------------------------
-    //--------Functions--------
-    //-------------------------
-
-
-    //-------------------------
-    //----static Functions-----
-    //-------------------------
-    public static Number(value: any): number;
-
-    public static Object(value: any): Object;
-
-    public static cerr(text: string): void;
-
-    public static cflush(): void;
-
-    public static cout(text: string): void;
-
-    public static decodeURI(encodedURI: string): string;
-
-    public static decodeURIComponent(encodedURI: string): string;
-
-    public static encodeURI(URI: string): string;
-
-    public static encodeURIComponent(uriComp: string): string;
-
-    public static eval(s: string): Object;
-    public static eval(s: string, object: object): Object;
-
-    public static format(fmt: string): string;
-    public static format(...value: any[]): string;
-
-    public static gc(): void;
-    public static gc(hardGC: boolean /* true*/): void;
-
-    public static gcBytes(): number;
-
-    public static getEnvironmentVariable(varName: string): string;
-
-    public static isFinite(n: number): boolean;
-
-    public static isNaN(value: any): boolean;
-
-    public static loadResource(filePath: string): boolean;
-    public static loadResource(filePath: string, rootPath: string): boolean;
-
-    public static matchesWildSpecification(what: string, wildPattern: string): boolean;
-
-    public static msleep(milliseconds: number): void;
-
-    public static parseFloat(s: string): number;
-
-    public static parseInt(s: string): number;
-    public static parseInt(s: string, radix: number): number;
-
-    public static processEvents(): void;
-    public static processEvents(excludeUserInputEvents: boolean /* false*/): void;
-    public static processEvents(excludeUserInputEvents: boolean /* false*/, iterations: number /* 1*/): void;
-
-    public static replaceEnvironmentVariables(s: string): string;
-
-    public static searchDirectory(dirPath: string): Array<any>;
-    public static searchDirectory(dirPath: string, recursive: boolean /* false*/): Array<any>;
-
-    public static sleep(seconds: number): void;
-
-    public static systemOffsetFromUTC(): number;
-    public static systemOffsetFromUTC(date: Date | string): number;
-
-    public static unloadResource(filePath: string): boolean;
-    public static unloadResource(filePath: string, rootPath: string): boolean;
-
-
-
-    //-------------------------
-    //----static Properties----
-    //-------------------------
-
-
-    //-------------------------
-    //--------constants--------
-    //-------------------------
-}
+/* declare function Number(value: any): number; */
+/* declare function Object(value: any): Object; */
+declare function cerr(text: string): void;
+declare function cflush(): void;
+declare function cout(text: string): void;
+declare function decodeURI(encodedURI: string): string;
+declare function decodeURIComponent(encodedURI: string): string;
+declare function encodeURI(URI: string): string;
+declare function encodeURIComponent(uriComp: string): string;
+declare function eval(s: string): Object;
+declare function eval(s: string, object: object): Object;
+declare function format(fmt: string): string;
+declare function format(...value: any[]): string;
+declare function gc(): void;
+declare function gc(hardGC: boolean /* true*/): void;
+declare function gcBytes(): number;
+declare function getEnvironmentVariable(varName: string): string;
+declare function isFinite(n: number): boolean;
+declare function isNaN(value: any): boolean;
+declare function loadResource(filePath: string): boolean;
+declare function loadResource(filePath: string, rootPath: string): boolean;
+declare function matchesWildSpecification(what: string, wildPattern: string): boolean;
+declare function msleep(milliseconds: number): void;
+declare function parseFloat(s: string): number;
+declare function parseInt(s: string): number;
+declare function parseInt(s: string, radix: number): number;
+declare function processEvents(): void;
+declare function processEvents(excludeUserInputEvents: boolean /* false*/): void;
+declare function processEvents(excludeUserInputEvents: boolean /* false*/, iterations: number /* 1*/): void;
+declare function replaceEnvironmentVariables(s: string): string;
+declare function searchDirectory(dirPath: string): Array<any>;
+declare function searchDirectory(dirPath: string, recursive: boolean /* false*/): Array<any>;
+declare function sleep(seconds: number): void;
+declare function systemOffsetFromUTC(): number;
+declare function systemOffsetFromUTC(date: Date | string): number;
+declare function unloadResource(filePath: string): boolean;
+declare function unloadResource(filePath: string, rootPath: string): boolean;
